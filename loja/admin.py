a86019ec0ee52e09b5bec982c6e8c1ba29f06a2c
Isaque <isaque.s@escolar.ifrn.edu.br>
2026-04-06 11:27:56 -0300

    categoria e produto adicionados

--- loja/admin.py
+++ loja/admin.py
@@ -4,3 +4,5 @@ from django.contrib import admin
 
 from .models import * #importa nossos models
 admin.site.register(Fabricante) #adiciona a interface do adm
+admin.site.register(Categoria)
+admin.site.register(Produto)
